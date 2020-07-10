9982ae57b328ce76f195b9dccbdefd5cbe8bf7a7
Chunwei Tian <chunweitian@163.com>
2020-07-11 00:55:29 +0800

    Add files via upload

---
+++ lesrcnn_b/train.py
@@ -0,0 +1,55 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import json
+import argparse
+import importlib
+from solver import Solver
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--ckpt_name", type=str)
+    
+    parser.add_argument("--print_interval", type=int, default=1000) #original data is 1000
+    parser.add_argument("--train_data_path", type=str, 
+                        default="dataset/DIV2K_train.h5")
+    parser.add_argument("--ckpt_dir", type=str,
+                        default="checkpoint")
+    parser.add_argument("--sample_dir", type=str,
+                        default="sample/")
+    
+    parser.add_argument("--num_gpu", type=int, default=1)
+    parser.add_argument("--shave", type=int, default=20)
+    parser.add_argument("--scale", type=int, default=2)
+
+    parser.add_argument("--verbose", action="store_true", default="store_true")
+
+    parser.add_argument("--group", type=int, default=1)
+    
+    parser.add_argument("--patch_size", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--max_steps", type=int, default=200000)
+    parser.add_argument("--decay", type=int, default=150000)
+    parser.add_argument("--lr", type=float, default=0.0001)
+    parser.add_argument("--clip", type=float, default=10.0)
+
+    #parser.add_argument("--loss_fn", type=str, 
+                        #choices=["MSE", "L1", "SmoothL1"], default="L1") #tcw201904082029
+    parser.add_argument("--loss_fn", type=str, 
+                        choices=["MSE", "L1", "SmoothL1"], default="MSE")
+    return parser.parse_args()
+
+def main(cfg):
+    # dynamic import using --model argument
+    net = importlib.import_module("model.{}".format(cfg.model)).Net
+    print(json.dumps(vars(cfg), indent=4, sort_keys=True))
+    
+    solver = Solver(net, cfg)
+    #print 'ds'
+    solver.fit()
+    #print 'sdsddwdew'
+
+if __name__ == "__main__":
+    cfg = parse_args()
+    main(cfg)
